7f763af58efe9b391dde35d959fef24b6413bcc7
leonardobarrachi <73356095+leonardobarrachi@users.noreply.github.com>
2022-06-23 15:39:11 -0300

    Adicionei arquivos via upload

---
+++ Hello GitHub/src/br/com/leonardobarrachi/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.leonardobarrachi.git;
+
+/**
+ * @author CAVERNA
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
